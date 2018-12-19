cfeacf5b17557f704466d9301ba6572d6e0d707b
liuyuxuan123 <1010134430@qq.com>
2018-12-18 23:36:06 -0500

    Rasterize !

--- ValleyVoxel/Sources/Layers/Layers.swift
+++ ValleyVoxel/Sources/Layers/Layers.swift
@@ -28,3 +28,55 @@ class BorderedLayer: CALayer{
         super.init(coder: aDecoder)
     }
 }
+
+class ColoredLayer: BorderedLayer {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(color: UIColor) {
+        super.init()
+        self.backgroundColor = color.cgColor
+    }
+}
+
+class TexturedLayer: BorderedLayer {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(image: UIImage) {
+        super.init()
+        
+        self.contents = image.cgImage
+        self.contentsGravity = CALayerContentsGravity.resizeAspectFill
+        self.masksToBounds = true
+        self.shouldRasterize = true
+    }
+}
+
+class GradientedLayer: CAGradientLayer {
+    
+    override var frame: CGRect {
+        didSet { borderWidth = frame.width / 20 }
+    }
+    
+    init(colors: [UIColor]) {
+        super.init()
+        self.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        self.borderColor = UIColor.black.cgColor
+        self.colors = colors.compactMap { $0.cgColor }
+        self.shouldRasterize = true
+    }
+    
+    convenience init(colors: [UIColor], start: CGPoint = .zero, end: CGPoint) {
+        self.init(colors: colors)
+        self.startPoint = start
+        self.endPoint = end
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
